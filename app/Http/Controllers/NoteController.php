7ead044a218c69e30c9b31a48ea164401d010649
HassanBook <hassanabdullah40@gmail.com>
2024-07-08 02:58:52 -0700

    fix bugs

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Note;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NoteController extends Controller
 {
@@ -12,6 +13,9 @@ class NoteController extends Controller
      */
     public function index()
     {
+        if (!Auth::check()) {
+            return redirect()->route('login'); // Redirect to login page
+        }
         $notes = Note::
             where('user_id', request()->user()->id)
             ->orderBy('created_at', 'desc')
